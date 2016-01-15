56d6df8e014464ad5ca20631c231ee4cf3d3cdb6
Justin Pounders <justin_pounders@uml.edu>
2016-01-15 10:58:54 -0500

    Updated and simplified install

--- pyne_install_ubuntu14.sh
+++ pyne_install_ubuntu14.sh
@@ -25,7 +25,7 @@ cd opt
 # Install MOAB
 mkdir moab
 cd moab
-#git clone https://bitbucket.org/fathomteam/moab
+git clone https://bitbucket.org/fathomteam/moab
 cd moab
 git checkout Version4.8.2
 autoreconf -fi
@@ -52,8 +52,8 @@ cd ..
 # Install PyNE
 git clone https://github.com/pyne/pyne.git
 cd pyne
-# python setup.py install --user -- -DMOAB_LIBRARY=$HOME/opt/moab/lib -DMOAB_INCLUDE_DIR=$HOME/opt/moab/include
-python setup.py install --user
+python setup.py install --user -- -DMOAB_LIBRARY=$HOME/opt/moab/lib -DMOAB_INCLUDE_DIR=$HOME/opt/moab/include
+# python setup.py install --user
 echo "export PATH=$HOME/.local/bin:\$PATH" >> ~/.bashrc
 echo "export LD_LIBRARY_PATH=$HOME/.local/lib:\$LD_LIBRARY_PATH" >> ~/.bashrc
 echo "alias build_pyne='python setup.py install --user -- -DMOAB_LIBRARY=\$HOME/opt/moab/lib -DMOAB_INCLUDE_DIR=\$HOME/opt/moab/include'" >> ~/.bashrc
